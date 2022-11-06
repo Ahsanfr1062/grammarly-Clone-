6733804b3220795a7a824c80701ef00ee8d9e720
Ahsanfr <darklight1062@gmail.com>
2022-11-06 17:02:07 +0500

    Adding all the website content

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        70: "17.4rem",
+
+      }
+    },
+  },
+  plugins: [],
+}
